a5288bb7d3957297756d957e3cf33ab8c4dfb99f
Kurt M. Ma. Coll <kurt@ubx.ph>
2023-05-23 17:16:10 +0800

    Pave way for DB migration

---
+++ knexfile.ts
@@ -0,0 +1,22 @@
+// Update with your config settings.
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+/**
+ * @type { Object.<string, import("knex").Knex.Config> }
+ */
+export default {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: process.env.DB_HOST || '127.0.0.1',
+      port: parseInt(process.env.DB_PORT || '5432'),
+      database: process.env.DB_NAME || 'todo_local',
+      user: process.env.DB_USERNAME || 'postgres',
+      password: process.env.DB_PASSWORD || ''
+    }
+  },
+
+};
