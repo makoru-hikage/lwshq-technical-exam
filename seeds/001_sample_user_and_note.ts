ef4853a09113810e20dc22715e09cafae242ff6b
Kurt M. Ma. Coll <kurt@ubx.ph>
2023-05-29 19:17:28 +0800

    Update seeder file

---
+++ seeds/001_sample_user_and_note.ts
@@ -0,0 +1,22 @@
+import {Knex} from 'knex';
+import UserRepository from '../src/modules/users/adapters/repository';
+import NoteRepository from '../src/modules/notes/adapters/repository';
+
+export async function seed(knex: Knex): Promise<any> {
+  const userRepo = new UserRepository(knex);
+
+  const user = await userRepo.createUser({
+    name: 'Isabela Bettor',
+    email: 'sample@test.com',
+    password: 'P@ssw0rd'
+  });
+
+  const noteRepo = new NoteRepository(knex);
+
+  return noteRepo.create({
+    user_id: user.id,
+    title: 'Sample Note',
+    text: 'This is a sample note.'
+
+  })
+}
