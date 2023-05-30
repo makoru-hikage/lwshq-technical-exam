b916b83da3d5f4243a457ae2431e84cee56e39b4
Kurt M. Ma. Coll <kurt@ubx.ph>
2023-05-30 22:11:43 +0800

    Fix the JWT auth plugin

--- src/plugins/jwt/index.ts
+++ src/plugins/jwt/index.ts
@@ -1,7 +1,12 @@
 import fastifyJwt from '@fastify/jwt';
 import fp from 'fastify-plugin';
-import { FastifyPluginAsync } from 'fastify'
+import { FastifyPluginAsync, FastifyReply, FastifyRequest } from 'fastify';
 
+export type UserPayload = {
+  id: string;
+  name: string;
+  email: string;
+};
 
 /**
  * This plugin enables the use of cookies.
@@ -11,11 +16,24 @@ import { FastifyPluginAsync } from 'fastify'
 const jwtPlugin: FastifyPluginAsync = fp(async (fastify) => {
   await fastify.register(fastifyJwt, {
     secret: process.env.JWT_SECRET || 'S3Cr3t!!',
+    cookie: {
+      cookieName: 'logged_user',
+      signed: false, // Set to true if you are using something like PGP
+    }
   });
 
-  fastify.log.info('COOKIE PLUGIN LOADED');
+  fastify.decorate('authenticate', async (request: FastifyRequest, reply: FastifyReply) => {
+    try {
+      await request.jwtVerify();
+    } catch (err) {
+      reply.code(401).send({ message: 'Access denied' });
+      throw new Error('Access denied');
+    }
+  });
+
+  fastify.log.info('JWT PLUGIN LOADED');
 }, {
-  name: 'cookie'
+  name: 'jwt-plugin'
 })
 
 export default jwtPlugin;
@@ -23,8 +41,15 @@ export default jwtPlugin;
 // When using .decorate you have to specify added properties for Typescript
 declare module 'fastify' {
   interface FastifyInstance {
-    decode: any;
-    sign: any;
-    verify: any;
+    decode: (jwt: string) => Promise<any>;
+    sign: (input: string | object) => Promise<string>;
+    verify: (jwt: string) => Promise<boolean>;
+    authenticate: (request: FastifyRequest, reply: FastifyReply) => Promise<void>;
   }
+}
+
+declare module '@fastify/jwt' {
+	export interface FastifyJWT {
+		user: UserPayload;
+	}
 }
