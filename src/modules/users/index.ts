911e303264dadc4d84b002e1e09fbf8ae6a51c6a
Kurt M. Ma. Coll <kurt@ubx.ph>
2023-05-30 22:03:50 +0800

    Rename module

--- src/modules/users/index.ts
+++ src/modules/users/index.ts
@@ -2,11 +2,11 @@ import UserController from "./controller";
 
 import { FastifyPluginAsync } from 'fastify';
 
-const userModule: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
+const UserModule: FastifyPluginAsync = async (fastify, opts): Promise<void> => {
 
   const controller = UserController(fastify);
 
   fastify.post('/login', controller.login);
 }
 
-export default userModule;
+export default UserModule;
