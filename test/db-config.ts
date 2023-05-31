a2ef2764971284336662cdc16a05b2bb8335e245
Kurt M. Ma. Coll <kurt@ubx.ph>
2023-05-31 22:55:54 +0800

    Make it more useful

--- test/db-config.ts
+++ test/db-config.ts
@@ -1,8 +1,9 @@
 import dotenv from 'dotenv';
+import Knex from 'knex';
 
 dotenv.config();
 
-export default {
+export const testKnexConfig = {
   client: 'pg',
   connection: {
     host: process.env.DB_HOST || '127.0.0.1',
@@ -10,5 +11,23 @@ export default {
     database: process.env.DB_NAME || 'test_todo_local',
     user: process.env.DB_USERNAME || 'postgres',
     password: process.env.DB_PASSWORD || ''
+  }
+}
+
+export const knex = Knex(testKnexConfig);
+
+export async function runMigrationAndSeeder(): Promise<void> {
+  try {
+    // Run migrations
+    await knex.migrate.latest();
+
+    // Run seeders
+    await knex.seed.run({
+      specific: '001_sample_user_and_note.ts',
+    });
+
+    console.log('Migration and seeding completed successfully!');
+  } catch (error) {
+    console.error('Error running migration and seeding:', error);
   }
 }
